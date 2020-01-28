cab8f9c5c6051e06e2b9e2553a75d76ef2975c42
Edgar Albalate <dev.gardo@gmail.com>
2020-01-28 10:23:12 +0100

    Stylized code

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,15 @@ use std::{thread, time};
 use rand::Rng;
 
 fn main() {
-    let mut xs: [String; 8] = [
-        "var".to_string(),
-        "std".to_string(),
-        "usr".to_string(),
-        "lib".to_string(),
-        "dev".to_string(),
-        "bin".to_string(),
-        "opt".to_string(),
-        "tmp".to_string()
-    ];
+    let mut xs = vec!["var", "std", "usr", "lib", "dev", "bin", "opt", "tmp"];
+    let r = vec!["Fail".red(), "Ok".green()];
     let mut rng = rand::thread_rng();
     loop {
-        let i: usize = rand::thread_rng().gen_range(1, 9);
-        let hun_millis = time::Duration::from_millis((i as u64) * 100);
-        thread::sleep(hun_millis);
+        let i: usize = rng.gen_range(1, 9);
+        let spaces = " ".repeat(4 * (8 - i));
+        let status = match rng.gen_range(0, 16) { 0 => &r[0], _ => &r[1] };
         xs.shuffle(&mut rng);
-        let status = {
-            if rand::thread_rng().gen_range(0, 16) == 0 {
-                "Fail".red()
-            } else { "Ok".green() }
-        };
-        println!("  /{}/ {} {}", &xs[0..i].join("/"), " ".repeat(4 * (8 - i)), status);
+        thread::sleep(time::Duration::from_millis((i as u64) * 100));
+        println!("  /{}/ {} {}", &xs[0..i].join("/"), spaces, status);
     }
 }
