be093790be0516da77fbb33dc16ac9a5684f22dd
Edgar Albalate <dev.gardo@gmail.com>
2020-01-30 10:18:08 +0100

    Added more styled output

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use colored::*;
 use rand::seq::SliceRandom;
 use std::{thread, time};
+use chrono::Local;
 use rand::Rng;
 
 fn main() {
     let mut xs = vec!["var", "std", "usr", "lib", "dev", "bin", "opt", "tmp"];
-    let r = vec!["Fail".red(), "Ok".green()];
+    let r = vec!["Err".red(), "Ok".green()];
     let mut rng = rand::thread_rng();
     loop {
         let i: usize = rng.gen_range(1, 9);
+        let time = Local::now().format("%Y-%m-%d %H:%M:%S");
+        let path = &xs[0..i].join("/").blue();
         let spaces = " ".repeat(4 * (8 - i));
         let status = match rng.gen_range(0, 16) { 0 => &r[0], _ => &r[1] };
         xs.shuffle(&mut rng);
-        thread::sleep(time::Duration::from_millis((i as u64) * 100));
-        println!("  /{}/ {} {}", &xs[0..i].join("/"), spaces, status);
+        thread::sleep(time::Duration::from_millis((9 + (i as u64)) * 100));
+        println!("{}\n  /{}/ {} {}", time, path, spaces, status);
     }
 }
