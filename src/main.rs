5d4b3f539a3611c8876131e0b2b6ea95f0f5f9dd
Edgar Albalate <dev.gardo@gmail.com>
2021-06-01 11:26:16 +0200

    Changed err/ok randrange small format changes

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ use rand::Rng;
 
 fn main() {
     let mut xs = vec!["var", "std", "usr", "lib", "dev", "bin", "opt", "tmp"];
-    let r = vec!["Err".red(), "Ok".green()];
+    let r = vec!["Err".red(), " Ok".green()];
     let mut rng = rand::thread_rng();
     loop {
         let i: usize = rng.gen_range(1, 9);
         let time = Local::now().format("%Y-%m-%d %H:%M:%S");
         let path = &xs[0..i].join("/").blue();
         let spaces = " ".repeat(4 * (8 - i));
-        let status = match rng.gen_range(0, 16) { 0 => &r[0], _ => &r[1] };
+        let status = match rng.gen_range(0, 8) { 0 => &r[0], _ => &r[1] };
         xs.shuffle(&mut rng);
         thread::sleep(time::Duration::from_millis((9 + (i as u64)) * 100));
         println!("{}\n  /{}/ {} {}", time, path, spaces, status);
